a150f07d431c3e6ec77b21be7fc6b37524f17db4
oguzzhany <oguzzhan@me.com>
2023-05-08 17:20:40 +0200

    with map function

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,12 @@ const tweetsArray = [
 function App() {
   return (
     <div className='App'>
-      <Tweet tweet={ tweetsArray[0] } />
+      {/* <Tweet tweet={ tweetsArray[0] } />
       <Tweet tweet={ tweetsArray[1] } />
-      <Tweet tweet={ tweetsArray[2] } />
+      <Tweet tweet={ tweetsArray[2] } /> */}
+      {tweetsArray.map((tweet) => (
+      <Tweet tweet={ tweet } />
+      ))}
     </div>
   )
 }

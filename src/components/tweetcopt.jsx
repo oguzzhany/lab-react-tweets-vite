30fb3afe9f915a20365e18532ea3a04291f3eab8
oguzzhany <oguzzhan@me.com>
2023-05-08 17:07:47 +0200

    finished lab

---
+++ src/components/tweetcopt.jsx
@@ -0,0 +1,39 @@
+function Tweet({tweet}) {
+    return (
+      <div className="tweet">
+        <img
+          src="https://i.imgur.com/9yw1Fyw.jpg"
+          className="profile"
+          alt="profile"
+        />
+  
+        <div className="body">
+          <div className="top">
+            <span className="user">
+              <span className="name">{tweet.user.name}</span>
+              <span className="handle">{tweet.user.handle}</span>
+            </span>
+  
+            <span className="timestamp">{tweet.timestamp}</span>
+          </div>
+  
+          <p className="message">
+            {tweet.message}
+          </p>
+  
+          <div className="actions">
+            {/* Font Awesome icons */}
+            <i className="far fa-comment"></i>
+            <i className="fas fa-retweet"></i>
+            <i className="far fa-heart"></i>
+            <i className="fas fa-share"></i>
+          </div>
+        </div>
+  
+        <i className="fas fa-ellipsis-h"></i>
+      </div>
+    );
+  }
+  
+  export default Tweet;
+  
